60234f9cd4782cfe4deeb43a531d96a0721e0a4a
yujaeman <yooman22@naver.com>
2018-12-06 15:17:10 +0900

    4일차

--- app/src/main/java/com/example/yujaeman/howl/LoginActivity.kt
+++ app/src/main/java/com/example/yujaeman/howl/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.example.yujaeman.howl
 
+import android.content.Context
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
@@ -11,13 +12,27 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.*
 import kotlinx.android.synthetic.main.activity_login.*
+import android.content.pm.PackageManager
+import com.google.android.gms.common.util.ClientLibraryUtils.getPackageInfo
+import android.content.pm.PackageInfo
+import android.util.Base64
+import android.util.Log
+import android.webkit.WebView
+import com.facebook.*
+import com.facebook.appevents.AppEventsLogger
+import com.facebook.internal.FacebookSignatureValidator
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
+import java.security.MessageDigest
+import java.util.*
+
 
 class LoginActivity : AppCompatActivity() {
 
     var auth : FirebaseAuth? = null
     var googleSignInClient : GoogleSignInClient? = null
     var GOOGLE_LOGIN_CODE = 9001
-
+    var callbackManager : CallbackManager?= null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,6 +47,11 @@ class LoginActivity : AppCompatActivity() {
             googleLogin()
         }
 
+        facebook_login_button.setOnClickListener { view->
+            facebookLogin()
+        }
+
+
 
 
         var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
@@ -40,6 +60,22 @@ class LoginActivity : AppCompatActivity() {
             .build()
 
         googleSignInClient = GoogleSignIn.getClient(this,gso)
+        callbackManager = CallbackManager.Factory.create()
+        //printHashKey(this)
+    }
+
+    fun printHashKey(context: Context) {
+        try {
+            val info = context.packageManager.getPackageInfo(context.packageName, PackageManager.GET_SIGNATURES)
+            for (signature in info.signatures) {
+                val md = MessageDigest.getInstance("SHA")
+                md.update(signature.toByteArray())
+                val hashKey = String(Base64.encode(md.digest(), 0))
+                Log.i("AppLog", "key:$hashKey=")
+            }
+        } catch (e: Exception) {
+            Log.e("AppLog", "error:", e)
+        }
 
     }
 
@@ -94,6 +130,7 @@ class LoginActivity : AppCompatActivity() {
         startActivityForResult(signIntent,GOOGLE_LOGIN_CODE)
     }
 
+
     fun firebaseAuthWithGoogle(account : GoogleSignInAccount)
     {
         var credential = GoogleAuthProvider.getCredential(account.idToken,null)
@@ -102,8 +139,38 @@ class LoginActivity : AppCompatActivity() {
         this.finish()
     }
 
+    fun facebookLogin()
+    {
+        LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("public_profile","email"))
+        LoginManager.getInstance().registerCallback(callbackManager,object :FacebookCallback<LoginResult>{
+            override fun onSuccess(result: LoginResult?) {
+                handleFaacebookAccessToken(result?.accessToken)
+                //startActivity(Intent(this@LoginActivity,MainActivity::class.java))
+            }
+
+            override fun onCancel() {
+
+
+            }
+
+            override fun onError(error: FacebookException?) {
+                Log.d("error",error.toString())
+            }
+
+        })
+    }
+
+    fun handleFaacebookAccessToken(token:AccessToken?)
+    {
+        var credential = FacebookAuthProvider.getCredential(token?.token!!)
+        auth?.signInWithCredential(credential)
+    }
+
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
+        callbackManager?.onActivityResult(requestCode,resultCode,data)
+
 
         if(GOOGLE_LOGIN_CODE == requestCode) {
             var result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
--- app/src/main/java/com/example/yujaeman/howl/MainActivity.kt
+++ app/src/main/java/com/example/yujaeman/howl/MainActivity.kt
@@ -2,11 +2,28 @@ package com.example.yujaeman.howl
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        LogOut_Button.setOnClickListener {
+            view ->
+            FirebaseAuth.getInstance().signOut()
+
+            var message : String = "Logout 성공"
+            Toast.makeText(this,message,message.length)
+
+        }
+
     }
+
+
+
+
 }
