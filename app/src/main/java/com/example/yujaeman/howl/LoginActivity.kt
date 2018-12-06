3099d64df35108c990ae55ddb8bf6dcc3af4fd75
yujaeman <yooman22@naver.com>
2018-12-06 10:41:18 +0900

    3일차

--- app/src/main/java/com/example/yujaeman/howl/LoginActivity.kt
+++ app/src/main/java/com/example/yujaeman/howl/LoginActivity.kt
@@ -4,38 +4,64 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.*
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     var auth : FirebaseAuth? = null
+    var googleSignInClient : GoogleSignInClient? = null
+    var GOOGLE_LOGIN_CODE = 9001
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         auth = FirebaseAuth.getInstance()
+
         email_login_button.setOnClickListener { view ->
             createAndLoginEmail()
         }
 
+        google_sign_in_button.setOnClickListener { view->
+            googleLogin()
+        }
+
+
+
+        var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this,gso)
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+       FirebaseAuth.getInstance().signOut()
     }
 
     fun createAndLoginEmail()
     {
         auth?.createUserWithEmailAndPassword(email_edittext.text.toString(),password_edittext.text.toString())?.addOnCompleteListener{
-            task -> if(task.isSuccessful){
-                Toast.makeText(this,"아이디 생성 성공",Toast.LENGTH_LONG).show()
-            }
-                else if(task.exception?.message.isNullOrEmpty())
-            {
-                Toast.makeText(this,"아이디 생성 실패",Toast.LENGTH_LONG).show()
-            }
-            else
-            {
-                emailLogin()
-            }
+                task -> if(task.isSuccessful){
+            Toast.makeText(this,"아이디 생성 성공",Toast.LENGTH_LONG).show()
+        }
+        else if(task.exception?.message.isNullOrEmpty())
+        {
+            Toast.makeText(this,"아이디 생성 실패",Toast.LENGTH_LONG).show()
+        }
+        else
+        {
+            emailLogin()
+        }
         }
     }
 
@@ -61,5 +87,39 @@ class LoginActivity : AppCompatActivity() {
             this.finish()
         }
     }
+
+    fun googleLogin()
+    {
+        var signIntent = googleSignInClient?.signInIntent
+        startActivityForResult(signIntent,GOOGLE_LOGIN_CODE)
+    }
+
+    fun firebaseAuthWithGoogle(account : GoogleSignInAccount)
+    {
+        var credential = GoogleAuthProvider.getCredential(account.idToken,null)
+        auth?.signInWithCredential(credential)
+        startActivity(Intent(this,MainActivity::class.java))
+        this.finish()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(GOOGLE_LOGIN_CODE == requestCode) {
+            var result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
+            if (result.isSuccess) {
+                var account = result.signInAccount
+                firebaseAuthWithGoogle(account!!)
+            }
+            else
+            {
+                var message : String = "구글 연결에 실패하였습니다"
+                Toast.makeText(applicationContext,message,message.length)
+            }
+        }
+    }
+
+
+
 
 }
